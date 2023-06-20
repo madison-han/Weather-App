9afb6575fc6a1c1b463b77f6496f5b48493ce97f
madison-han <104019938+madison-han@users.noreply.github.com>
2023-06-20 00:49:32 -0400

    Create userInterface.java

---
+++ userInterface.java
@@ -0,0 +1,6 @@
+public class UserInterface {
+    public static void displayWeatherInformation(String city, String weatherData) {
+        System.out.println("Weather information for " + city + ":");
+        System.out.println(weatherData);
+    }
+}
